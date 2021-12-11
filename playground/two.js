bbec79b120364d081f36e4c64f834ed8ebc33044
valoronions <79231711+valoronions@users.noreply.github.com>
2021-12-11 10:56:28 -0500

    added minutes

--- playground/two.js
+++ playground/two.js
@@ -1,5 +1,6 @@
 let seconds = 00;
 let milliseconds = 00;
+let minutes = 00;
 let interval;
 let isRunning = false;
 let splits = []
@@ -7,6 +8,7 @@ let splits = []
 
 const millisecondsText = document.getElementById('milliseconds');
 const secondsText= document.getElementById('seconds');
+const minutesText = document.getElementById('minutes');
 const swstartstop = document.querySelector('.sw-start-stop');
 const swreset = document.querySelector('.sw-reset');
 const splitsText = document.querySelector('.splits');
@@ -28,12 +30,16 @@ const restartStopwatch = () => {
     clearInterval(interval);
     milliseconds = "00";
     seconds = "00";
+    minutes = "00";
     millisecondsText.innerHTML = milliseconds;
     secondsText.innerHTML = seconds;
+    minutesText.innerHTML = minutes;
+    isRunning = false;
+    swstartstop.innerHTML = "Start";
 }
 
 const addSplit = () => {
-    splits.push(" " + seconds + ":" + milliseconds);
+    splits.push(" " + minutes + ":" + seconds + ":" + milliseconds);
     splitsText.innerHTML = splits;
 }
 
@@ -63,4 +69,12 @@ const timerLogic = () => {
     if (seconds > 9) {
         secondsText.innerHTML = seconds;
     }
+
+    if (seconds > 59){
+        minutes++;
+        minutesText.innerHTML = "0" + minutes;
+        seconds = 0;
+        secondsText.innerHTML = "0" + 0;
+    }
+    
 }
